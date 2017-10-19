3b55c350a17e92813be8050afbefdcaa66bcc28b
yaniragl <yanira_galeas@hotmail.com>
2017-10-19 18:59:29 -0300

    Initial commit

---
+++ application/views/formulario.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/estilos.css">
+
+</head>
+<body>
+
+<div id="bg">
+  <div class="module">
+    <ul>
+      <li class="tab activeTab"><img src="https://i.imgur.com/Fk1Urva.png" alt="" class="icon"/></li>
+      <li class="tab" ><img src="https://i.imgur.com/ZsRgIDD.png" alt="" class="icon"/></li>
+      <li class="tab" ><img src="https://i.imgur.com/34Q50wo.png" alt="" class="icon"/></li>
+      <li class="tab" ><img src="https://i.imgur.com/LCCJ06E.png" alt="" class="icon"/></li>
+    </ul>
+    
+    <form class="form">
+      <input type="text" placeholder="primer nombre" class="textbox" />
+      <input type="text" placeholder="Apellido" class="textbox" />
+      <input type="text" placeholder="Direccion de Email" class="textbox" />
+      <input type="button" value="Next" class="button" />
+    </form>
+  </div>
+</div>
+
+<a href="http://dribbble.com/shots/1265587-Registration-Template-PSD?list=everyone" target="_blank">Design by: Asif Aleem</a>
+
+</body>
+</html>
